75c6e181436949f03549014097276f101708dfa0
Divya R <kwrdivyaraj100@gmail.com>
2023-10-05 14:30:07 +0530

    Employee table created

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const express = require('express');
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "root123"
+    password: "root123",
+    database: "nodemysql"
 
 });
 
@@ -33,13 +34,24 @@ app.get('/createdb', (req, res)=>{
         res.send('Database created')
     })
 })
-// app.get('/createemployee', (req, res)=>{
-//     let sql = 'CREATE TABLE employee(id int AUTO_INCREMENT, name VARCHAR(50), designation VARCHAR(50), PRIMARY KEY(id))';
-//     db.query(sql,error=>{
+app.get('/createemployee', (req, res)=>{
+    let sql = 'CREATE TABLE employee(id int AUTO_INCREMENT, name VARCHAR(50), designation VARCHAR(50), PRIMARY KEY(id))';
+    db.query(sql,error=>{
+        if(error){
+            return error
+        }
+        res.send('Employee Table created')
+    })
+})
+// app.get('/employee1', (req, res)=>{
+//     let post = {name:"Radha", designation:"Teacher"};
+//     let sql = 'INSERT INTO employee SET ?';
+//     let query = db.query
+//     db.query(sql,post,error=>{
 //         if(error){
 //             return error
 //         }
-//         res.send('Employee Table created')
+//         res.send('Employee added');
 //     })
 // })
 
